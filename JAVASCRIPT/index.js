da01d663ec96c55b8adff33834c3eb2cf1287b21
slppills <sunchaehoon2@naver.com>
2024-07-27 17:51:09 +0900

    perf: js

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -2,6 +2,11 @@ const homeWrapper = document.querySelector(".home-wrapper");
 let inputValue = document.getElementById("title-input");
 const categoryItems = document.querySelectorAll(".header-navbar li");
 const categorySpan = document.getElementById("category-span");
+const languageToggle = document.getElementById("chk1");
+const categoryList = document.getElementById("category-list");
+const modal = document.querySelector("dialog");
+let isLanguageKorean = true;
+let category = "popular";
 let moviedata;
 let prevCategory = "";
 
@@ -23,6 +28,7 @@ const fetchAndDisplayMovies = (url) => {
     .then((response) => response.json())
     .then((response) => {
       moviedata = [...response.results];
+      console.log(moviedata);
       displayMovies(moviedata);
     })
     .catch((err) => console.error(err));
@@ -33,26 +39,98 @@ const displayMovies = (movies) => {
   movies.forEach((movie) => {
     const moviePoster = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`;
     const movieList = `
-      <div class="movie-box">
-        <div class="movie-box-wrapper">
-          <img src="${moviePoster}" alt="${movie.title}">
-          <span>${movie.title}</span>
+      <div class="movie-box" id=${movie.id}>
+        <div class="movie-box-wrapper" id=${movie.id}>
+          <img src="${moviePoster}" alt="${movie.title}" id=${movie.id}>
+          <span id=${movie.id}>${movie.title}</span>
         </div>
       </div>`;
     homeWrapper.innerHTML += movieList;
   });
+
+  document.querySelectorAll(".movie-box").forEach((box) => {
+    box.addEventListener("click", (e) => {
+      getModalMovie(e.target.id);
+      modal.showModal();
+      document.body.style.overflow = "hidden";
+    });
+  });
 };
 
-fetchAndDisplayMovies("https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1");
+fetchAndDisplayMovies(`https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`);
+
+const getModalMovie = (movieId) => {
+  const modalWrapper = document.querySelector(".modal");
+  const modalLoading = `
+    <div class="modal-loading"><span>Loading...</span></div>
+  `;
+  modalWrapper.innerHTML = modalLoading;
+  fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?language=${isLanguageKorean === true ? "ko-KR" : "en-UN"}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const modalMoviedata = response;
+      console.log(modalMoviedata);
+      const moviePoster = `https://image.tmdb.org/t/p/w500/${modalMoviedata.backdrop_path}`;
+
+      const MovieInfo = `
+        <div class="modal-image">
+          <img src=${moviePoster} alt=${modalMoviedata.title}>
+          <div class="modal-genres">${modalMoviedata.genres.map((genre) => `<span>#${genre.name}</span>`)}</div>
+        </div>
+        <div class="modal-info">
+          <div class="modal-info-wrapper">
+            <h1>${modalMoviedata.title}</h1>
+            <div class="modal-releasedate">
+              <span>${modalMoviedata.release_date}</span>
+              <div><span class="vote_average">${modalMoviedata.vote_average.toFixed(1)}</span>/10</div>
+            </div>
+            <span class="modal-overview">${
+              modalMoviedata.overview ? modalMoviedata.overview : "(언어를 바꿔주세요)"
+            }</span>
+          </div>
+          
+        </div>
+      `;
+      modalWrapper.innerHTML = MovieInfo;
+    })
+    .catch((err) => console.error(err));
+};
+
+modal.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) modal.close();
+  document.body.style.overflow = "auto";
+});
 
 inputValue.addEventListener("input", (e) => {
-  const searchTitle = moviedata.filter((movie) => movie.title.includes(e.target.value));
+  const searchTerm = e.target.value.toLowerCase().replace(/\s+/g, "");
+  const searchTitle = moviedata.filter((movie) => movie.title.toLowerCase().replace(/\s+/g, "").includes(searchTerm));
   displayMovies(searchTitle);
 });
 
-const categoryList = document.getElementById("category-list");
 categoryList.addEventListener("click", (e) => {
-  const category = e.target.id;
+  category = e.target.id;
   fetchAndDisplayMovies(`https://api.themoviedb.org/3/movie/${e.target.id}?language=ko-KR&page=1`);
   categorySpan.innerText = document.getElementById(category).innerText;
+});
+
+languageToggle.addEventListener("click", () => {
+  isLanguageKorean = !isLanguageKorean;
+  isLanguageKorean === true
+    ? fetchAndDisplayMovies(`https://api.themoviedb.org/3/movie/${category}?language=ko-KR&page=1`)
+    : fetchAndDisplayMovies(`https://api.themoviedb.org/3/movie/${category}?language=en-UN&page=1`);
+});
+
+document.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY; // 현재 스크롤 위치
+  const viewportHeight = window.innerHeight; // 뷰포트 높이
+  const docHeight = document.documentElement.scrollHeight; // 전체 페이지 높이
+
+  if (scrollTop + viewportHeight >= docHeight) {
+    isLanguageKorean === true
+      ? fetchAndDisplayMovies(`https://api.themoviedb.org/3/movie/${category}?language=ko-KR&page=1`)
+      : fetchAndDisplayMovies(`https://api.themoviedb.org/3/movie/${category}?language=en-UN&page=1`);
+  }
 });
